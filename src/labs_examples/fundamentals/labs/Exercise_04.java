8e56aeca118baf046100c41cede68ffafe15206e
tinayi08 <christinayi827@gmail.com>
2020-11-23 22:45:01 -0800

    update

--- src/labs_examples/fundamentals/labs/Exercise_04.java
+++ src/labs_examples/fundamentals/labs/Exercise_04.java
@@ -13,8 +13,21 @@ public class Exercise_04 {
     public static void main(String[] args) {
 
         int i = 1;
+        boolean boo = true;
+        short s = 1;
+        long l = 34;
+        float f = 34.1f;
+        double d = 45.333;
+        byte b = 3;
+        char c = 'c';
         System.out.println("int i is: " + i);
-
+        System.out.println("boolean boo is: " + boo);
+        System.out.println("short s is: " + s);
+        System.out.println("long l is: " + l);
+        System.out.println("float f is: " + f);
+        System.out.println("double d is: " + d);
+        System.out.println("byte b is: " + b);
+        System.out.println("char c is: " + c);
         // write your code below
 
     }
--- src/labs_examples/fundamentals/labs/Exercise_05.java
+++ src/labs_examples/fundamentals/labs/Exercise_05.java
@@ -14,15 +14,21 @@ public class Exercise_05 {
 
         String str = "hello!";
         // please declare an int variable below, and set it to the value of the length of "str"
-
+        int length = str.length();
         String str2 = "hello";
         // please initialize a boolean variable and test whether str is equal to str2
-
-        // please concatenate str & str2 and set the result to a new String variable below
-
+        boolean b = str.equals(str2);
+        // please concatenate str & str2 and set the result to a new String variable belowS
+        String s = str + " " + str2;
         // please demonstrate the use of any other method that is available to us in the String class
         // for example, replace(), substring(), contains(), indexOf() etc
+        String newString = str.substring(5,6);
 
+        System.out.println(str);
+        System.out.println(str2);
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(newString);
     }
 
 
