31951c3e871050a2ef7bd0141324ee74d8d61784
tinayi08 <christinayi827@gmail.com>
2021-01-26 20:41:39 -0800

    completed excercise 1 - Excercise 2 is completed and uploaded to the BlackJack repo

---
+++ src/labs_examples/static_nonstatic/labs/Class1.java
@@ -0,0 +1,34 @@
+package labs_examples.static_nonstatic.labs;
+
+public class Class1 {
+
+    public static void main(String[] args) {
+        staticMethod();
+
+        Class1 object = new Class1();
+        object.originalNonStatic();
+
+        Class2.staticDifClass();
+        Class2 object2 = new Class2();
+        object2.nonStaticDifClass();
+    }
+
+
+    public static void staticMethod() {
+        System.out.println("this is a static method from the same class");
+    }
+
+    public void originalNonStatic() {
+        System.out.println("\nThis is a nonstatic method from the same class");
+        anotherNonStatic();
+        Class2 obj = new Class2();
+        obj.nonStaticDifClass();
+        Class2.staticDifClass();
+    }
+
+    public void anotherNonStatic() {
+        System.out.println("We added another non static method being called to a non static method.");
+        staticMethod();
+    }
+
+}
---
+++ src/labs_examples/static_nonstatic/labs/Class2.java
@@ -0,0 +1,16 @@
+package labs_examples.static_nonstatic.labs;
+
+public class Class2 {
+    public static void main(String[] args) {
+
+    }
+
+
+    public static void staticDifClass() {
+        System.out.println("\nWe are calling a static method from a different class.");
+    }
+
+    public void nonStaticDifClass() {
+        System.out.println("Calling non-static method from different class");
+    }
+}
